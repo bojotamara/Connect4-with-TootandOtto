bf2ec3ad98a7febd1ab3223dee652b9f629daf13
bojotamara <tamarabojovic@hotmail.com>
2020-04-01 01:54:45 -0600

    Added rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,28 +1,28 @@
+#![feature(decl_macro)]
+#![feature(proc_macro_hygiene)]
 
-#[macro_use]
-extern crate bson;
-use mongodb::{Client, options::ClientOptions};
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate bson;
+use mongodb::{Client};
+
+static mut MC: Option<Client> = None;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/hello/<name>")]
+fn hi(name: String) -> String {
+    name
+}
 
 fn main() -> Result<(), mongodb::error::Error> {
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017")?;
-
-    // Manually set an option.
-    client_options.app_name = Some("My App".to_string());
-
-    // Get a handle to the deployment.
-    let client = Client::with_options(client_options)?;
-
-    // List the names of the databases in that deployment.
-    for db_name in client.list_database_names(None)? {
-        println!("{}", db_name);
+    unsafe {
+        let client = Client::with_uri_str("mongodb://localhost:27017/")?;
+        MC = Some(client);
     }
 
-    // Get a handle to a database.
-    let db = client.database("mydb");
-    // List the names of the collections in that database.
-    for collection_name in db.list_collection_names(None)? {
-        println!("{}", collection_name);
-    }
-
+    rocket::ignite().mount("/", routes![hello, hi]).launch();
     Ok(())
 }
